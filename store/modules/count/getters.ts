46c0a8fb982bd245b2bab8d102c805f271335a18
Mohammad Ali <mdali2016.227@gmail.com>
2022-01-17 10:01:58 +0600

    [Change] renamed helper.ts

--- store/modules/count/getters.ts
+++ store/modules/count/getters.ts
@@ -1,4 +1,4 @@
-import helpers from '../../../helpers/helpers';
+import helper from '../../../helpers/helper';
 
 export default {
   allInfo: (state: any) => {
@@ -6,40 +6,40 @@ export default {
     return [
       {
         title: 'TOTAL CASES',
-        quantity: helpers.convertToAccountingFormat(state.todayInfo.cases),
+        quantity: helper.convertToAccountingFormat(state.todayInfo.cases),
         date,
         difference: ((state.todayInfo.cases/state.lastDayInfo.cases-1) * 100).toFixed(2),
         hasIncreased: state.todayInfo.cases > state.lastDayInfo.cases,
         isSuccess: state.todayInfo.cases === state.lastDayInfo.cases,
-        lastDayQuantity: helpers.convertToMultiplier(state.lastDayInfo.cases, 1)
+        lastDayQuantity: helper.convertToMultiplier(state.lastDayInfo.cases, 1)
       },
       {
         title: 'TOTAL DEATHS',
-        quantity: helpers.convertToAccountingFormat(state.todayInfo.deaths),
+        quantity: helper.convertToAccountingFormat(state.todayInfo.deaths),
         date,
         difference: ((state.todayInfo.deaths/state.lastDayInfo.deaths - 1) * 100).toFixed(2),
         hasIncreased: state.todayInfo.deaths > state.lastDayInfo.deaths,
         isSuccess: state.todayInfo.deaths === state.lastDayInfo.deaths,
-        lastDayQuantity: helpers.convertToMultiplier(state.lastDayInfo.deaths, 1)
+        lastDayQuantity: helper.convertToMultiplier(state.lastDayInfo.deaths, 1)
       },
       {
         title: 'TOTAL RECOVERIES',
-        quantity: helpers.convertToAccountingFormat(state.todayInfo.recovered),
+        quantity: helper.convertToAccountingFormat(state.todayInfo.recovered),
         date,
         difference: ((state.todayInfo.recovered/state.lastDayInfo.recovered - 1) * 100).toFixed(2),
         hasIncreased: state.todayInfo.recovered > state.lastDayInfo.recovered,
         isSuccess: state.todayInfo.recovered > state.lastDayInfo.recovered,
-        lastDayQuantity: helpers.convertToMultiplier(state.lastDayInfo.recovered, 1)
+        lastDayQuantity: helper.convertToMultiplier(state.lastDayInfo.recovered, 1)
       },
       {
         title: 'ACTIVE CASES',
-        quantity: helpers.convertToAccountingFormat(state.todayInfo.active),
+        quantity: helper.convertToAccountingFormat(state.todayInfo.active),
         date,
         difference: ((state.todayInfo.active/state.lastDayInfo.active - 1) * 100).toFixed(2),
         hasIncreased: state.todayInfo.active > state.lastDayInfo.active,
         isSuccess: state.todayInfo.active < state.lastDayInfo.active,
         isDanger: state.todayInfo.active > state.lastDayInfo.active,
-        lastDayQuantity: helpers.convertToMultiplier(state.lastDayInfo.active, 1)
+        lastDayQuantity: helper.convertToMultiplier(state.lastDayInfo.active, 1)
       },
     ]
   }
