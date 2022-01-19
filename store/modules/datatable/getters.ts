80392030522540b4a597b95ecbe71ffdb4edcb6c
Mohammad Ali <mdali2016.227@gmail.com>
2022-01-19 11:02:45 +0600

    [Change] mapped country info inside mutations instead of getters for faster filtering.

--- store/modules/datatable/getters.ts
+++ store/modules/datatable/getters.ts
@@ -3,7 +3,7 @@ import helper from '../../../helpers/helper';
 export default {
   rowsPerPage: (state:any):number => state.rowsPerPage,
   currentPage: (state:any):number => state.page,
-  totalPages: (state:any, getters:any, rootState:any):number => Math.ceil(state.allCountryInfo.length/state.rowsPerPage),
+  totalPages: (state:any):number => Math.ceil(state.allCountryInfo.length/state.rowsPerPage),
 
   globalInfo: (state: any, getters:any, rootState:any):{} => ({
     cases: helper.convertToAccountingFormat(rootState.count.todayInfo.cases),
@@ -22,31 +22,13 @@ export default {
     let dataSet = state.yesterday ? [...state.lastDayAllCountryInfo] : [...state.allCountryInfo]
     //Set Ranks according to cases
     return dataSet
-      .sort((a:any, b:any) => b.cases-a.cases)
-      .map((country:any, index:number) => ({...country, rank: index+1}))
       //Filter dataset according to country search
-      .filter((dataSet: { country: string }): boolean => dataSet.country.includes(state.country) || state.country === "")
+      .filter((dataSet: { name: string }): boolean =>
+        state.country === "" || dataSet.name.toLowerCase().includes(state.country.toLowerCase()))
       //Sort filtered Data By Rank
       .sort((a:any, b:any) => a.rank-b.rank)
       //Take 10, 25, 50, 100 per page
       .slice((state.page-1)*state.rowsPerPage)
       .slice(0, state.rowsPerPage)
-      //Send specific arrays only
-      .map((info:any) => ({
-        rank: info.rank,
-        flag: info.countryInfo.flag,
-        name: info.country,
-        cases: helper.convertToAccountingFormat(info.cases),
-        deaths: helper.convertToAccountingFormat(info.deaths),
-        recovered: helper.convertToAccountingFormat(info.recovered),
-        todayCases: (info.todayCases ? '+':'')
-          +helper.convertToAccountingFormat(info.todayCases),
-        todayDeaths: (info.todayDeaths ? '+':'')
-          +helper.convertToAccountingFormat(info.todayDeaths),
-        todayRecovered: (info.todayRecovered ? '+':'')
-          +helper.convertToAccountingFormat(info.todayRecovered),
-        casesPerOneMillion: helper.convertToAccountingFormat(info.casesPerOneMillion),
-        deathsPerOneMillion: helper.convertToAccountingFormat(info.deathsPerOneMillion)
-      }))
   }
 }
--- store/modules/datatable/mutations.ts
+++ store/modules/datatable/mutations.ts
@@ -1,9 +1,47 @@
+import helper from "~/helpers/helper";
+
 export default  {
-  UPDATE_ALL_COUNTRY_INFO(state: any, data: {}): void {
+  UPDATE_ALL_COUNTRY_INFO(state: any, data: []): void {
     state.allCountryInfo = data
+      .sort((a:any, b:any) => b.cases-a.cases)
+      //Send specific arrays only with rank
+      .map((info:any, index:number) => ({
+        rank: index+1,
+        flag: info.countryInfo.flag,
+        name: info.country,
+        cases: helper.convertToAccountingFormat(info.cases),
+        deaths: helper.convertToAccountingFormat(info.deaths),
+        recovered: helper.convertToAccountingFormat(info.recovered),
+        todayCases: (info.todayCases ? '+':'')
+          +helper.convertToAccountingFormat(info.todayCases),
+        todayDeaths: (info.todayDeaths ? '+':'')
+          +helper.convertToAccountingFormat(info.todayDeaths),
+        todayRecovered: (info.todayRecovered ? '+':'')
+          +helper.convertToAccountingFormat(info.todayRecovered),
+        casesPerOneMillion: helper.convertToAccountingFormat(info.casesPerOneMillion),
+        deathsPerOneMillion: helper.convertToAccountingFormat(info.deathsPerOneMillion)
+      }))
   },
-  UPDATE_LAST_DAY_ALL_COUNTRY_INFO(state: any, data: {}): void {
+  UPDATE_LAST_DAY_ALL_COUNTRY_INFO(state: any, data: []): void {
     state.lastDayAllCountryInfo = data
+      .sort((a:any, b:any) => b.cases-a.cases)
+      //Send specific arrays only with rank
+      .map((info:any, index:number) => ({
+        rank: index+1,
+        flag: info.countryInfo.flag,
+        name: info.country,
+        cases: helper.convertToAccountingFormat(info.cases),
+        deaths: helper.convertToAccountingFormat(info.deaths),
+        recovered: helper.convertToAccountingFormat(info.recovered),
+        todayCases: (info.todayCases ? '+':'')
+          +helper.convertToAccountingFormat(info.todayCases),
+        todayDeaths: (info.todayDeaths ? '+':'')
+          +helper.convertToAccountingFormat(info.todayDeaths),
+        todayRecovered: (info.todayRecovered ? '+':'')
+          +helper.convertToAccountingFormat(info.todayRecovered),
+        casesPerOneMillion: helper.convertToAccountingFormat(info.casesPerOneMillion),
+        deathsPerOneMillion: helper.convertToAccountingFormat(info.deathsPerOneMillion)
+      }))
   },
   UPDATE_COUNTRY_INPUT(state: any, text: string): void {
     state.country = text
