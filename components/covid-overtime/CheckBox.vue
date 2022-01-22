781723e1664ed77e9fb9ff6bda86af929c6ef2ec
Mohammad Ali <mdali2016.227@gmail.com>
2022-01-22 21:25:36 +0600

    [Change] Check box style

--- components/covid-overtime/CheckBox.vue
+++ components/covid-overtime/CheckBox.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-lg mt-2" style="max-width: 80vh" :class="{ 'dark-primary': darkMode, 'light-primary': !darkMode}">
-    <span v-for="(content, i) in dataContentList" :key="i" class="mr-3">
+    <label v-for="(content, i) in dataContentList" :key="i" class="mr-3">
       <input type="checkbox" v-model="dataContent" @change="updateMapContent"
-             :value="content.value"> <span>{{ content.name }}</span>
-    </span>
+             :class="content.value" :value="content.value"> <span class="p-1">{{ content.name }}</span>
+    </label>
     <p class="description">This is a depiction of the spread of COVID-19 over time. We rely on the Johns Hopkins CSSE Data Repository, which is updated once a day at around 23:59 UTC. For that reason, the most recent data our slider allows users to select is yesterday's.</p>
   </div>
 </template>
@@ -45,5 +45,38 @@ export default {
   font-size:12px;
   text-align:center;
   padding: 2rem;
+}
+input[type=checkbox] {
+  -moz-appearance:none;
+  -webkit-appearance:none;
+  outline: none;
+  content: none;
+}
+input[type=checkbox]:before {
+  content: "\f00c";
+  font-size: 15px;
+  color: transparent;
+  display: block;
+  width: 25px;
+  height: 25px;
+  border-radius: 5px;
+}
+input.cases:before {
+  border: 2px solid red;
+}
+input.deaths:before {
+  border: 2px solid black;
+}
+input.recovered:before {
+  border: 2px solid green;
+}
+input.cases:checked:before {
+  background: red;
+}
+input.deaths:checked:before {
+  background: black;
+}
+input.recovered:checked:before {
+  background: green;
 }
 </style>
